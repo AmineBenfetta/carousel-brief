ec55a53d6923642ea58a43f9c7d348ee4f1919ba
Amine.B <amine.benfetta711@gmail.com>
2025-05-19 16:18:54 +0200

    feat (script.js): implement slide show functionality with navigation

--- boilerplate7-1/js/script.js
+++ boilerplate7-1/js/script.js
@@ -1,1 +1,29 @@
+let slideIndex = 1;
+const slides = document.querySelectorAll('.slides');
 
+function showSlide(n) {
+    if (n > slides.length) {
+        n = 1;
+    } else if (n < 1) {
+        n = slides.length;
+    }
+    slideIndex =n;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex-1].style.display = 'block';
+    const currentSlide = slides[slideIndex - 1];
+    const counter = currentSlide.querySelector('.num');
+    const text = slideIndex + '/' + slides.length;
+    counter.textContent = text;
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    showSlide(slideIndex);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+});
